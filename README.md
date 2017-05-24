afdbd34d382c35e3e7b175e7bb65d34d9da603fb
Anish Mishra <amishr0@SHI58495EU1138S.kih.kmart.com>
2017-05-24 20:23:36 +0530

    new files

---
+++ README.md
@@ -0,0 +1,9 @@
+# react.js shopping cart
+
+
+install dependencies and start local dev server
+
+```sh
+npm install
+npm start
+```
---
+++ src/data/products.js
@@ -0,0 +1,53 @@
+const data = [
+  {
+    id: 1,
+    name: 'product A',
+    price: 499.99,
+    currency: '$',
+    image: 'images/text.png',
+    url: 'http://placehold.it/150/1b21a0/ffffff'
+  },
+  {
+    id: 2,
+    name: 'product B',
+    price: 249.99,
+    currency: '$',
+    image: 'images/text_1.png',
+    url: 'http://placehold.it/150/bc4125/ffffff'
+  },
+  {
+    id: 3,
+    name: 'product C',
+    price: 239.99,
+    currency: '$',
+    image: 'images/text_2.png',
+    url: 'http://placehold.it/150/5abc25/ffffff'
+  },
+  {
+    id: 4,
+    name: 'product D',
+    price: 119.99,
+    currency: '$',
+    image: 'images/text_3.png',
+    url: 'http://placehold.it/150/ebf742/ffffff'
+  },
+  {
+    id: 5,
+    name: 'product E',
+    price: 599.99,
+    currency: '$',
+    image: 'images/text_4.png',
+    url: 'http://placehold.it/150/d121c8/fffff'
+  },
+  {
+    id: 6,
+    name: 'product F',
+    price: 399.99,
+    currency: '$',
+    image: 'images/text.png',
+    url: 'http://placehold.it/150/1b21a0/ffffff'
+  }
+
+];
+
+export default data;
